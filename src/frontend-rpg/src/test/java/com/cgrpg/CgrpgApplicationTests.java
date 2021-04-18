5b0a36dff65e2122e5f4ea8ad39ee96b11934b4e
niilzon <nquairiat@gmail.com>
2021-04-18 21:26:15 +0200

    Initial commit - basic project structure

---
+++ src/frontend-rpg/src/test/java/com/cgrpg/CgrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgrpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
