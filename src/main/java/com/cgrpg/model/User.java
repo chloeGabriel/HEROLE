43f0839837f13c559988e02ba4d6bbfbe67a4325
niilzon <nquairiat@gmail.com>
2021-04-24 10:53:43 +0200

    fixed hibernate jointable mapping issue for USER_ROLE

--- src/main/java/com/cgrpg/model/User.java
+++ src/main/java/com/cgrpg/model/User.java
@@ -8,6 +8,8 @@ import java.util.Set;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Timestamp created;
@@ -23,13 +25,11 @@ public class User {
     private Timestamp resetTokenExpiration;
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(	name = "user_roles",
+    @JoinTable(	name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
